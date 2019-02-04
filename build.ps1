30c9873624d22436861370766c2a89015a36b57b
Andrey Kolomentsev <andrey.kolomentsev@gmail.com>
2019-02-04 12:59:47 -0800

    support multiple platforms

--- build.ps1
+++ build.ps1
@@ -1,4 +1,18 @@
+param(
+    [Parameter()]
+    [ValidateSet("centos","win1809","win1803")]
+    [string] $target = "centos"
+
+)
+
 $ErrorActionPreference = "Stop"
-docker build -t simple-rest -f .\Dockerfile.win .
-docker tag simple-rest akolomentsev/simple-rest
-docker push akolomentsev/simple-rest
+
+push-location $psScriptRoot
+
+$dockerFile = gci "Dockerfile.$target"
+
+docker build -t akolomentsev/simple-rest:$target -f $dockerFile .
+docker push akolomentsev/simple-rest:$target
+
+pop-location
+
